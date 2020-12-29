0cf868423811bf1b5975b0c65403c1df684a9197
Karsten Lloyd <development@karsten-lloyd.dev>
2020-12-29 13:55:42 +0000

    Added search field and applied styles Added search by pressing enter or the search button

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,53 @@
+import React, { useState } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function Home() {
+  const [searchterm, setSearchterm] = useState('')
+
+  // Update search term
+  const updateTerm = (e) => { setSearchterm(e.target.value) };
+
+  // Search if enter is pressed
+  const checkEnter = (e) => {
+    if (e.keyCode === 13) {
+      search()
+    }
+  }
+
+  // Search for an artist
+  const search = (e) => {
+    console.log('search for artist')
+  }
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Artist Song Stats</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className={styles.main}>
+        <h1>Artist Search</h1>
+        <p>Search and click on a result to view the artist's song stats</p>
+        <div className={styles.search}>
+          <input
+            type='text' 
+            name='search'
+            placeholder='Search for an artist'
+            autoFocus={true}
+            value={searchterm}
+            onChange={updateTerm}
+            onKeyUp={checkEnter}
+          />
+          <button title="Search" onClick={search}>
+            <FontAwesomeIcon icon={faSearch}/>
+          </button>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </div>
     </div>
   )
 }
